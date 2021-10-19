95caec84e20c76ba137ce8ebb7c9d16e9b3ff48b
Christoph Kiesewetter <63119652+Chris-Kiese@users.noreply.github.com>
2021-10-19 18:58:39 +0200

    Added basic account model

---
+++ lib/src/model/account/account.dart
@@ -0,0 +1,22 @@
+class Account {
+  int? id;
+  String name;
+  String password;
+  String username;
+
+  Account(this.name, this.username, this.password, [this.id]);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'username': username,
+      'password': password,
+    };
+  }
+
+  @override
+  String toString() {
+    return "id : $id| name: $name| username: $username | password: $password \n";
+  }
+}
