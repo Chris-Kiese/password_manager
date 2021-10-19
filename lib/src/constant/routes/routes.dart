cf7e5fe67e4fe2bf3e8ee7b5cf1939748b31aaeb
Christoph Kiesewetter <63119652+Chris-Kiese@users.noreply.github.com>
2021-10-19 19:32:30 +0200

    created file for routing

--- lib/src/constant/routes/routes.dart
+++ lib/src/constant/routes/routes.dart
@@ -1,1 +1,14 @@
-//TODO: create
+import 'package:password_manager/src/pages/home/home.dart';
+
+const homeRoute = '/';
+const settingsRoute = '/settings';
+const vaultRoute = '/vault';
+const generatorRoute = '/generator';
+
+//TODO: Configure Routes after pages are created.
+final routes = {
+  homeRoute: (context) => const HomePage()
+  //vaultRoute: (context) => VaultPage(),
+  //generatorRoute: (context) => GeneratorPage(),
+  //settingsRoute: (context) => SettingsPage(),
+};
