fd5d40bc43cb3d62a6b3a1dfe2652082d167649e
Christoph Kiesewetter <63119652+Chris-Kiese@users.noreply.github.com>
2021-10-19 19:27:42 +0200

    Created very basic home page

--- lib/src/pages/home/home.dart
+++ lib/src/pages/home/home.dart
@@ -1,1 +1,63 @@
-//TODO: create
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: _buildAppBar(), body: _buildBody());
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      title: const Text('Password Manager'),
+      centerTitle: true,
+      elevation: 10.0,
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(
+            Icons.settings,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            //TODO: push settings route
+          },
+        )
+      ],
+    );
+  }
+
+  _buildBody() {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildVaultButton(),
+          const SizedBox(
+            height: 100,
+          ),
+          _buildGeneratorButton()
+        ],
+      ),
+    );
+  }
+
+  _buildVaultButton() {
+    return ElevatedButton(
+      child: const Text('Enter Vault'),
+      onPressed: () {
+        //TODO: enter vault page
+      },
+    );
+  }
+
+  _buildGeneratorButton() {
+    return ElevatedButton(
+      child: const Text('Enter Vault'),
+      onPressed: () {
+        //TODO: enter generator page
+      },
+    );
+  }
+}
